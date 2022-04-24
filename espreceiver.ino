64844af8bbf0715344ab004d689046ae57b3a732
hassanalisultani <91322408+hassanalisultani@users.noreply.github.com>
2022-04-24 22:38:18 +0400

    Add files via upload

---
+++ espreceiver.ino
@@ -0,0 +1,54 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+
+const char* ssid = "Coderatory_"; //"Applied Physics AP 5";
+const char* password = "q1werty6068"; //"Blackrose2017";
+
+// Create an instance of the server
+// specify the port to listen on as an argument
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi Connected");
+
+  // Start the server
+  server.begin();
+
+  // Print the IP address
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  // Wait until the client sends some data
+  while (!client.available()) {
+    delay(1);
+  }
+
+  // Read the message
+  String msg = client.readString();
+  Serial.println(msg);
+  String message = msg;
+}
